a760962c1b2c778580ca354e2ff45e10057b27a5
kazimahmad22 <kazimahmad.business@gmail.com>
2025-03-17 16:46:34 +0500

    Final Changes

--- prototype/main.py
+++ prototype/main.py
@@ -12,8 +12,8 @@ while True:
 
     frame_resized = cv.resize(frame, (640, 480), interpolation=cv.INTER_AREA)
     frame_bw = cv.cvtColor(frame_resized, cv.COLOR_BGR2GRAY)
-    frame_denoise = cv.medianBlur(frame_resized, ksize=5)
-    frame_edges = cv.Canny(frame_resized, 100, 200)
+    frame_denoise = cv.medianBlur(frame_bw, ksize=5)
+    frame_edges = cv.Canny(frame_denoise, 100, 200)
 
     cv.imwrite('processed images/resized_frame.png', frame_resized)
     cv.imwrite('processed images/black_and_white_frame.png', frame_bw)
